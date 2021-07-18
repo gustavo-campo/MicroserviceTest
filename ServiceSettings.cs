93b2825082ba80490513f9598de9bdb6e8c61630
gustavo camporotondi <gustavo.campo@gmail.com>
2021-07-18 20:37:03 -0300

    commit inicial

---
+++ ServiceSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroservicesTest
+{
+    public class ServiceSettings
+    {
+        public string OpenWheaterHost { get; set; }
+
+        public string ApiKey { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Polly;
+
+namespace MicroservicesTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //almacenamos la ruta al endpoint de OpenWeather.Org en el appsettings
+            //la clave de la api esta almacenada como user secret
+            services.Configure<ServiceSettings>(Configuration.GetSection(nameof(ServiceSettings)));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroservicesTest", Version = "v1" });
+            });
+
+            //agregamos el servicio que creamos para consultar el endpoint como un nuevo hhtpclient indicando su tipo
+            //usamos polly y exponential backoff para los retry en caso de problemas con el endpoint y circuit break para frenar los retry
+            services.AddHttpClient<WeatherClient>()
+                .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))))
+                .AddTransientHttpErrorPolicy(builder => builder.CircuitBreakerAsync(3, TimeSpan.FromSeconds(10)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroservicesTest v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
