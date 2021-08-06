9384b8e04f551ae0113c2dbc83146a8c5a2a1820
gustavo camporotondi <gustavo.campo@gmail.com>
2021-08-06 18:21:44 -0300

    Mejoras en estrutura del proyecto

--- Weather.Api/Models/ServiceSettings.cs
+++ Weather.Api/Models/ServiceSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Weather.Api
+﻿namespace Weather.Api
 {
     public class ServiceSettings
     {
